a76c05ba0126bab680359b6cad11b9228ae0c311
ChickenPlays14 <chloe@chickenplays.co.uk>
2022-04-01 21:41:23 +0100

    Work on menus and randomstuff file function

--- main/chatprogram.py
+++ main/chatprogram.py
@@ -1,8 +1,21 @@
 # import random
+import time
 # import ranquestions.py
 import modules.menu
-from modules.menu import selection
 
-modules.menu.main()
+selection = None
 
-if selection == "2":
+while selection != "E":
+    modules.menu.main()
+
+    selection = input("Please choose an option from the menu: ")
+
+    if selection == "2":
+        while selection != "E":
+            modules.menu.randomstuff()
+            selection = input("Please choose an option from the menu: ")
+            if selection == "1":
+                file = open("randomstuff.txt", "a")
+                ranstwrite = input("Enter anything you want! ")
+                file.write(ranstwrite + "\n")
+                file.close()
--- modules/menu.py
+++ modules/menu.py
@@ -1,15 +1,26 @@
 # This file contains the functions used to create and access the menu in the main program
 
-selection = "0"
+selection = None
 
 
 def main():
+    print(" ")
     print("1) Start Chatting!")
     print("2) Access RandomStuff text file")
+    print("3) Play a game!")
     print("E) Exit")
-    global selection
-    selection = input("Please choose an option from the menu: ")
 
 
-main()
-print(selection)
+def games():
+    print(" ")
+    print("1) AoT")
+    print("B) Back")
+    print("E) Exit")
+
+
+def randomstuff():
+    print(" ")
+    print("1) Write to file")
+    print("2) Read file")
+    print("B) Back")
+    print("E) Exit")
